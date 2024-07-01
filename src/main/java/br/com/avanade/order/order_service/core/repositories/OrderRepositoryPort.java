a11a19f44cc98da7c26a7450cf661beee0f08dcf
Matheus Cardoso <eunaogostodetrabalhos123@gmail.com>
2024-07-01 02:22:16 -0300

    Removing unnecessary files

--- src/main/java/br/com/avanade/order/order_service/core/repositories/OrderRepositoryPort.java
+++
@@ -1,8 +0,0 @@
-package br.com.avanade.order.order_service.core.repositories;
-
-import br.com.avanade.order.order_service.core.model.Order;
-
-public interface OrderRepositoryPort {
-    Order saveOrder(Order order);
-    Order findOrderById(String orderId);
-}
--- src/main/java/br/com/avanade/order/order_service/core/repositories/OrderServicePort.java
+++
@@ -1,9 +0,0 @@
-package br.com.avanade.order.order_service.core.repositories;
-
-import br.com.avanade.order.order_service.core.model.Order;
-
-public interface OrderServicePort {
-    Order createOrder();
-    Order findOrderById(String orderId);
-    void updateOrderStatus(String orderId);
-}
