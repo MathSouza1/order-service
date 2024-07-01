0b89b77f05f6eb7d4f2d42bf13345a69dfbec638
Matheus Cardoso <eunaogostodetrabalhos123@gmail.com>
2024-07-01 14:03:25 -0300

    Implementing OrderProducerPort at the CreateOrderService

--- src/main/java/br/com/avanade/order/order_service/core/service/CreateOrderService.java
+++ src/main/java/br/com/avanade/order/order_service/core/service/CreateOrderService.java
@@ -2,19 +2,25 @@ package br.com.avanade.order.order_service.core.service;
 
 import br.com.avanade.order.order_service.core.domain.Order;
 import br.com.avanade.order.order_service.core.domain.OrderStatus;
+import br.com.avanade.order.order_service.core.dtos.OrderEvent;
 import br.com.avanade.order.order_service.core.ports.inbound.CreateOrderPort;
 import br.com.avanade.order.order_service.core.ports.outbound.CreateOrderRepositoryPort;
+import br.com.avanade.order.order_service.core.ports.outbound.OrderProducerPort;
 
 public class CreateOrderService implements CreateOrderPort {
 
     private final CreateOrderRepositoryPort orderRepository;
+    private final OrderProducerPort orderProducerPort;
 
-    public CreateOrderService(CreateOrderRepositoryPort orderRepository) {
+    public CreateOrderService(CreateOrderRepositoryPort orderRepository, OrderProducerPort orderProducerPort) {
         this.orderRepository = orderRepository;
+        this.orderProducerPort = orderProducerPort;
     }
 
     @Override
     public Order execute() {
-        return this.orderRepository.createOrder(new Order(OrderStatus.AGUARDANDO_ENVIO));
+        Order order = this.orderRepository.createOrder(new Order(OrderStatus.AGUARDANDO_ENVIO));
+        orderProducerPort.execute(new OrderEvent(order.getId()));
+        return order;
     }
 }
