d1082a6fdf0129783af91987f55346ec78581643
Matheus Cardoso <eunaogostodetrabalhos123@gmail.com>
2024-07-01 00:04:01 -0300

    Changing saveOrder to createOrder and removing params

--- src/main/java/br/com/avanade/order/order_service/core/repositories/OrderServicePort.java
+++ src/main/java/br/com/avanade/order/order_service/core/repositories/OrderServicePort.java
@@ -3,7 +3,7 @@ package br.com.avanade.order.order_service.core.repositories;
 import br.com.avanade.order.order_service.core.model.Order;
 
 public interface OrderServicePort {
-    Order saveOrder(Order order);
+    Order createOrder();
     Order findOrderById(String orderId);
     void updateOrderStatus(String orderId);
 }
