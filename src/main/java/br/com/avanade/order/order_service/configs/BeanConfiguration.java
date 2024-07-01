5658cccbe3478689528260e8ebf90e80b3503665
Matheus Cardoso <eunaogostodetrabalhos123@gmail.com>
2024-07-01 03:20:16 -0300

    Adding the Bean configuration for the FindOrderByIdService

--- src/main/java/br/com/avanade/order/order_service/configs/BeanConfiguration.java
+++ src/main/java/br/com/avanade/order/order_service/configs/BeanConfiguration.java
@@ -1,7 +1,10 @@
 package br.com.avanade.order.order_service.configs;
 
+import br.com.avanade.order.order_service.core.ports.inbound.FindOrderByIdPort;
 import br.com.avanade.order.order_service.core.ports.outbound.CreateOrderRepositoryPort;
+import br.com.avanade.order.order_service.core.ports.outbound.FindOrderRepositoryPort;
 import br.com.avanade.order.order_service.core.service.CreateOrderService;
+import br.com.avanade.order.order_service.core.service.FindOrderByIdService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,5 +13,10 @@ public class BeanConfiguration {
     @Bean
     public CreateOrderService orderService(CreateOrderRepositoryPort orderRepository) {
         return new CreateOrderService(orderRepository);
+    }
+
+    @Bean
+    public FindOrderByIdService orderByIdService(FindOrderRepositoryPort findOrderRepository) {
+        return new FindOrderByIdService(findOrderRepository);
     }
 }
