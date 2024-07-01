73a113e14919c8fefa4dfd59fc4d45f3bc49457e
Matheus Cardoso <eunaogostodetrabalhos123@gmail.com>
2024-07-01 03:09:21 -0300

    Throwing a OrderNotFoundException at the execute method

--- src/main/java/br/com/avanade/order/order_service/core/ports/inbound/FindOrderByIdPort.java
+++ src/main/java/br/com/avanade/order/order_service/core/ports/inbound/FindOrderByIdPort.java
@@ -1,7 +1,8 @@
 package br.com.avanade.order.order_service.core.ports.inbound;
 
 import br.com.avanade.order.order_service.core.domain.Order;
+import br.com.avanade.order.order_service.core.exceptions.OrderNotFoundException;
 
 public interface FindOrderByIdPort {
-    Order execute(String id);
+    Order execute(String id) throws OrderNotFoundException;
 }
