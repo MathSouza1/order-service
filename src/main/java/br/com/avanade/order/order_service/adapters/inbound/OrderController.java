23b231a87422ac405d3aa98a308fd6875d062069
Matheus Cardoso <eunaogostodetrabalhos123@gmail.com>
2024-07-01 02:50:33 -0300

    Implementing FindOrderById method

--- src/main/java/br/com/avanade/order/order_service/adapters/inbound/OrderController.java
+++ src/main/java/br/com/avanade/order/order_service/adapters/inbound/OrderController.java
@@ -2,11 +2,10 @@ package br.com.avanade.order.order_service.adapters.inbound;
 
 import br.com.avanade.order.order_service.core.domain.Order;
 import br.com.avanade.order.order_service.core.ports.inbound.CreateOrderPort;
+import br.com.avanade.order.order_service.core.ports.inbound.FindOrderByIdPort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RestController
@@ -14,13 +13,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderController {
 
     private final CreateOrderPort createOrder;
+    private final FindOrderByIdPort findById;
 
-    public OrderController(CreateOrderPort createOrder) {
+    public OrderController(CreateOrderPort createOrder, FindOrderByIdPort findById) {
         this.createOrder = createOrder;
+        this.findById = findById;
     }
 
     @PostMapping
     public ResponseEntity<Order> createOrder() {
         return ResponseEntity.ok().body(createOrder.execute());
+    }
+
+    @GetMapping("/{orderId}")
+    public ResponseEntity<Order> findOrderById(@PathVariable String orderId) {
+        return ResponseEntity.ok().body(findById.execute(orderId));
     }
 }
