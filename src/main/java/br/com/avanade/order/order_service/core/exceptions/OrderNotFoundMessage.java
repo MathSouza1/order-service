367f323ed48995a226f0134e67d88fc73a3311ac
Matheus Cardoso <eunaogostodetrabalhos123@gmail.com>
2024-07-02 19:07:44 -0300

    Removing unused methods

--- src/main/java/br/com/avanade/order/order_service/core/exceptions/OrderNotFoundMessage.java
+++ src/main/java/br/com/avanade/order/order_service/core/exceptions/OrderNotFoundMessage.java
@@ -2,35 +2,10 @@ package br.com.avanade.order.order_service.core.exceptions;
 
 import java.util.Date;
 
-
 public class OrderNotFoundMessage {
     private Date timestamp;
     private int status;
     private String error;
-
-    public Date getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(Date timestamp) {
-        this.timestamp = timestamp;
-    }
-
-    public int getStatus() {
-        return status;
-    }
-
-    public void setStatus(int status) {
-        this.status = status;
-    }
-
-    public String getError() {
-        return error;
-    }
-
-    public void setError(String error) {
-        this.error = error;
-    }
 
     public OrderNotFoundMessage(Date timestamp, int status, String error) {
         this.timestamp = timestamp;
@@ -39,6 +14,5 @@ public class OrderNotFoundMessage {
     }
 
     public OrderNotFoundMessage() {
-
     }
 }
