472743b18cec24240179be8754e2509d41298ed7
Matheus Cardoso <eunaogostodetrabalhos123@gmail.com>
2024-07-02 16:04:00 -0300

    Removing annotations because of the core of the service

--- src/main/java/br/com/avanade/order/order_service/core/exceptions/OrderNotFoundMessage.java
+++ src/main/java/br/com/avanade/order/order_service/core/exceptions/OrderNotFoundMessage.java
@@ -1,18 +1,44 @@
 package br.com.avanade.order.order_service.core.exceptions;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import java.util.Date;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
+
 public class OrderNotFoundMessage {
     private Date timestamp;
     private int status;
     private String error;
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public OrderNotFoundMessage(Date timestamp, int status, String error) {
+        this.timestamp = timestamp;
+        this.status = status;
+        this.error = error;
+    }
+
+    public OrderNotFoundMessage() {
+
+    }
 }
