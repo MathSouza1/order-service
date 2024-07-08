cb69440e9c0689f1b5829c29915fe065a8a984ec
Matheus Cardoso <eunaogostodetrabalhos123@gmail.com>
2024-07-08 17:03:24 -0300

    Adding the Getters and Setters again because of the ExceptionHandler

--- src/main/java/br/com/avanade/order/order_service/core/exceptions/OrderNotFoundMessage.java
+++ src/main/java/br/com/avanade/order/order_service/core/exceptions/OrderNotFoundMessage.java
@@ -1,6 +1,10 @@
 package br.com.avanade.order.order_service.core.exceptions;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Date;
+
 
 public class OrderNotFoundMessage {
     private Date timestamp;
@@ -14,5 +18,29 @@ public class OrderNotFoundMessage {
     }
 
     public OrderNotFoundMessage() {
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
     }
 }
